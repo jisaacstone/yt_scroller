c82c6a3f41594c73994362858a11856a761430d5
jisaacstone <jisaacstone@gmail.com>
2012-02-23 15:16:23 -0700

    editing ui

--- static/js/yt_edit.js
+++ static/js/yt_edit.js
@@ -4,7 +4,27 @@ $(function(){
             $(this).children('.edbox').css('display','block');
         }
     });
-    $('.edbox .submit').click(function(e){
+    $('.addnew').click(function(){
+        $('.overview p:first').clone(true)
+            .children('.textcontent').html('edit this text')
+            .siblings('.timestop').data('time','0')
+            .parent().insertAfter('.overview div:first');
+    });
+    $('.remove').click(function(){
+        $('.textcontent').click(function(){
+            $.ajax({
+                type: 'DELETE',
+                url: $(this).siblings('.ts-edit').data('url'),
+            });
+        });
+        $('.messagebox').css('display', 'block');
+        $('.messagebox .message').html('Click on Text to Remove');
+        $('.messagebox .close').click(function(){
+            $('.textcontent').unbind('click');
+            $('.messagebox').css('display', 'none');
+        });
+    });
+    $('p .edbox .submit').click(function(e){
         e.stopPropagation();
         time = $(this).siblings('input').val();
         html = $(this).parents('p').children('.textcontent').html();
@@ -19,4 +39,11 @@ $(function(){
         e.stopPropagation();
         $(this).parents('.edbox').css('display','none');
     });    
+    $('.hover').css('visibility','hidden');
+    $('.hover').parent().hover(function(){
+        $(this).children('.hover').css('visibility', 'visible');
+        }, function(){
+        $(this).children('.hover').css('visibility', 'hidden')
+            .children('.edbox').css('display', 'none');
+    });
 });
--- static/js/yt_effects.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.hover').css('visibility','hidden');
-    $('.hover').parent().hover(function(){
-        $(this).children('.hover').css('visibility', 'visible');
-        }, function(){
-        $(this).children('.hover').css('visibility', 'hidden');
-    });
-});
